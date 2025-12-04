8d6eac786edea77724c047ffefdf32182a53cc21
venkatraju <304venkatraju@gmail.com>
2025-12-04 08:46:54 +0530

    task2

---
+++ corejava/src/whileloop/whileloopprogram.java
@@ -0,0 +1,29 @@
+package whileloop;
+
+public class whileloopprogram {
+
+		    public static void main(String[] args) {
+
+		        int num = -5;  
+		        if (num > 0) {
+		            System.out.println("Positive");
+		        }
+		        else if (num < 0) {
+		            System.out.println("Negative");
+		        }
+		        else {
+		            System.out.println("Zero");
+		        }
+		    }
+		
+
+
+
+
+
+
+
+
+	}
+
+
